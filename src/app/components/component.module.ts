72198c69c6f99ca7423cec358d6b5f6f793ccba5
Victor Cornejo <rex2002xp@gmail.com>
2022-03-24 11:47:17 -0600

    feat(components): create navigation component (#4)

--- src/app/components/component.module.ts
+++ src/app/components/component.module.ts
@@ -1,17 +1,35 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LogoComponent } from './logo/logo.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 
 const COMPONENTS = [
-  LogoComponent
+  LogoComponent,
+  NavigationComponent
+]
+
+const MATERIALMODULES = [
+  MatToolbarModule,
+  MatButtonModule,
+  MatSidenavModule,
+  MatIconModule,
+  MatListModule
 ]
 
 @NgModule({
   declarations: [
-    ...COMPONENTS
+    ...COMPONENTS,
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    LayoutModule,
+    ...MATERIALMODULES
   ],
   exports: [
     ...COMPONENTS
